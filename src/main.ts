10d60ec60a3b98ee1027b602b0b0dc08a5d606ab
kyle <design210@nate.com>
2023-01-03 12:34:01 +0900

    globalProperties types

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import store from "./store/index.js";
 import vuetify from "./plugins/vuetify";
 import dayjs from 'dayjs';
 const app  = createApp(App);
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+      $dayjs: any;
+      $name:string;
+    }
+  }
+
 app.config.globalProperties.$dayjs = dayjs;
 app.config.globalProperties.$name = '글로벌 변수';
 app.use(router).use(store).use(vuetify).mount("#app");
